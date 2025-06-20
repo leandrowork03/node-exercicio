2c862993716aee306bf7e622d74ac2a51453b1d9
leandrowork03 <leandrowork03@gmail.com>
2025-06-20 18:32:22 -0300

    try and cacth

--- api/operacoes.js
+++ api/operacoes.js
@@ -2,16 +2,24 @@ const {soma, sub} = require('../services/operacoes')
 
 function apiSomar(req, res){
 
-    const {n1, n2} = req.body
+    try{
+        const {n1, n2} = req.body
     const somar = soma(n1,n2)
     res.status(200).send(somar)
+    }catch (e){
+        res.status(500).send(e.message)
+    }
 }
 
 function apiSub(req, res){
 
-    const {n1, n2} = req.body
+    try{
+        const {n1, n2} = req.body
     const subt = sub(n1,n2)
     res.status(200).send(subt)
+    }catch (e){
+        res.status(500).send(e.message)
+    }
 }
 
 module.exports={apiSomar, apiSub}
--- services/operacoes.js
+++ services/operacoes.js
@@ -1,7 +1,13 @@
 function soma(n1, n2){
+    if(isNaN(n1) || isNaN(n2)){
+        throw new Error ('apenas números')
+    }
     return n1 + n2
 }
 function sub(n1, n2){
+     if(isNaN(n1) || isNaN(n2)){
+        throw new Error ('apenas números')
+    }
     return n1 - n2
 }
 
